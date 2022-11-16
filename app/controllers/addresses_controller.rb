f5f07a6aefbbe767eee33c17b8f5f79adcf3f257
Janan Akkad <jananakkad@hotmail.co.uk>
2022-11-16 13:00:44 +0000

    commit tree 01af336147f58a452b79166d1fdd955c9531ad6d

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,3 @@
 class AddressesController < ApplicationController
+    
 end
--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,17 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
   end
 
   def create    
-    Person.create(person_params)
-    redirect_to people_path
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to people_path #or redirect to people_path(@person)
+    else
+      render :new
+    end
   end
 
   def index
@@ -15,6 +21,16 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(
+      :name,
+    :addresses_attributes => [
+      :street_address_1,
+      :street_address_2,
+      :city,
+      :state,
+      :zipcode,
+      :address_type
+    ]
+  )
   end
 end
