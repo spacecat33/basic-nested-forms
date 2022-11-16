137eb6486558316110f88bb7d3a2641fd181a5d6
Janan Akkad <jananakkad@hotmail.co.uk>
2022-11-16 16:00:42 +0000

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,11 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  accepts_nested_attributes_for :addresses
 
+  # the above method auto created the below method. Recommend writing out the below and not using the above shortcut:
+  # def addresses_attributes=(addresses_attributes) #Magic of nested forms
+  #   addresses_attributes.each do |address_attributes|
+  #     self. addresses.build(address_attributes)
+  #   end
+  # end
 end
